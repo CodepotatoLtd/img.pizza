f171002006b9159ec7179e718efdc0da6ae8590e
Gareth Thompson <gareth@codepotato.co.uk>
2019-12-19 14:54:33 +0000

    google tracking

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -40,5 +40,16 @@
 
     </div>
 
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-154776852-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-154776852-1');
+    </script>
+
+
 </body>
 </html>
